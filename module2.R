b4cb3163565a4b9e414c3ca81f86161e4c748178
Kevin <kevinjimenez@mail.usf.edu>
2022-01-23 15:49:07 -0500

    Added explanation to why code works/doesn't work

--- module2.R
+++ module2.R
@@ -6,3 +6,5 @@ return(sum(assignment2)/length(assignment2))
 #This function essentially gives the same output as the mean() function, as shown below
 myMean(assignment2)
 mean(assignment2)
+#This function works because it pulls the values from the assignment2 vector and runs the command accordingly. It would not work if we did the following code
+myMean
